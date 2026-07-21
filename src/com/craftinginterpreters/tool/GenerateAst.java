e28b10c7ca28600b8ef2ee23003c70014336fbc9
SKAT-16 <nathangetachew536@gmail.com>
2026-07-21 15:30:07 +0300

    Add AST node definitions for Assign and Var statements

--- src/com/craftinginterpreters/tool/GenerateAst.java
+++ src/com/craftinginterpreters/tool/GenerateAst.java
@@ -14,15 +14,18 @@ public class GenerateAst {
         String outputDir = args[0];
 
         defineAst(outputDir, "Expr", Arrays.asList(
+                "Assign   : Token name, Expr value",
                 "Ternary : Expr condition, Expr thenBranch, Expr elseBranch",
                 "Binary : Expr left, Token operator, Expr right",
                 "Grouping : Expr expression",
                 "Literal : Object value",
-                "Unary : Token operator, Expr right"));
+                "Unary : Token operator, Expr right",
+                "Variable : Token name"));
 
         defineAst(outputDir, "Stmt", Arrays.asList(
                 "Expression : Expr expression",
-                "Print      : Expr expression"));
+                "Print      : Expr expression",
+                "Var        : Token name, Expr initializer"));
     }
 
     private static void defineAst(
